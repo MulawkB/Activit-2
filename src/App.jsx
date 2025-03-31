2311ae7840157ec5424472f74770660817be100d
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-31 16:14:51 +0200

    import cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,38 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import { Col, Container, Row, Card } from "react-bootstrap";
 function App() {
-  const [product, setProduct] = useState(null);
+  const [product, setProduct] = useState([]);
   useEffect(() => {
     async function fetchProduct() {
-      const response = await fetch("https://fakestoreapi.com/products/1");
+      const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
       setProduct(data);
     }
     fetchProduct();
   }, []);
   return (
-    product && (
       <main>
         <Container>
-            <Card>
-              <Card.Img src={product.image} alt={product.title} />
-              <Card.Body>
-                <Card.Title>{product.title}</Card.Title>
-                <Card.Text>
-                  <strong>Prix :</strong> {product.price} €
-                </Card.Text>
-                {product.description}
-              </Card.Body>
-            </Card>
+          <Row>
+            {product.map((product) => (
+            <Col md={3} span={10} key={product.id}>
+              <Card>
+                <Card.Img src={product.image} alt={product.title} />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>
+                  {product.description}
+                  </Card.Text>
+                  <Card.Text>
+                    <strong>Prix :</strong> {product.price} €
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              </Col>
+              ))}
+          </Row>
         </Container>
       </main>
-    )
   );
 }
 
