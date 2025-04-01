d3fac684590a3d6385e9e9bc62d8c4f0104fc600
Brian Mulawka <mulawkabrian@gmail.com>
2025-04-01 09:46:16 +0200

    activitée 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row, Card, Button } from "react-bootstrap";
 function App() {
   const [products, setProducts] = useState([]);
   useEffect(() => {
@@ -11,9 +11,31 @@ function App() {
     }
     fetchProducts();
   }, []);
+
+  async function AddProducts() {
+    const response = await fetch("https://fakestoreapi.com/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Produit de test",
+        price: 29.99,
+        description: "Un super produit ajouté via API",
+        image: "https://via.placeholder.com/150",
+        category: "electronics",
+      }),
+    });
+    const data = await response.json();
+
+    alert("Le produit avec l'id " + data.id + " a été créé");
+  }
   return (
     <main>
       <Container className="my-4">
+        <Button variant="primary" className="mb-3" onClick={AddProducts}>
+          Ajouter un produit
+        </Button>
         <Row className="gy-3">
           {products.map((products) => (
             <Col md={3} lg={3} key={products.id}>
